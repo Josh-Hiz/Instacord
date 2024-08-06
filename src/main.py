1f294a35eba8af866ad15595d7cb9b89b7173e87
mattsolstice <mattsoltys7@gmail.com>
2024-08-05 20:55:56 -0400

    minor bug fix

--- src/main.py
+++ src/main.py
@@ -234,12 +234,12 @@ def run():
         brief = "Prints the current last post of designated account with downloading/deleting. Images within embed are permanent. If the channel/author used match a current post checker, it will overwrite that post checker. There will be no double post."
     )
     @has_permissions(ban_members=True)
-    async def lastpost_D(ctx, username, channel_id: int, role_id: int, label: str):
+    async def lastpost_D(ctx, username, channel_id: int, role_id: int):
         #global last_post_author, last_post
         
         global post_checkers
 
-        post_checkers = await post_data_D.lastpost_downloadv(bot, logger, ctx, username, channel_id, role_id, post_checkers, label)
+        post_checkers = await post_data_D.lastpost_downloadv(bot, logger, ctx, username, channel_id, role_id, post_checkers)
 
     
     @bot.command(brief="Lists all running post checkers")
