34a9d9847e8843a8d9f7f3a8a89b64a97d9eb0da
Josh H <jhizgiaev2704@gmail.com>
2024-08-31 01:18:51 -0400

    Update docs

--- src/post_data_ND.py
+++ src/post_data_ND.py
@@ -1,4 +1,3 @@
-
 ### post_data_ND
 ### functions to get post data without any downloading/deleting
 ### any image data will only temporarily remain in the post (48-168 hours, 2-7 days)
@@ -6,15 +5,25 @@
 ### to activate this files' post checker version, must replace function call in post_check_task() in main.py
 
 
-import instaloader
+from datetime import datetime
+
 import discord
+import instaloader
 from discord import app_commands
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions
-from datetime import datetime
 
 
-async def lastpost(ctx, username, channel_id: int, role_id: int, bot, logger, last_post, last_post_author):
+async def lastpost(
+    ctx,
+    username,
+    channel_id: int,
+    role_id: int,
+    bot,
+    logger,
+    last_post,
+    last_post_author,
+):
 
     target_channel = bot.get_channel(channel_id)
 
@@ -25,7 +34,7 @@ async def lastpost(ctx, username, channel_id: int, role_id: int, bot, logger, la
 
         profile_pic_url = profile.profile_pic_url
         fullname = profile.full_name
-        
+
         for post in profile.get_posts():
             if not post.is_pinned:
                 post_url = f"https://www.instagram.com/p/{post.shortcode}/"
@@ -34,12 +43,10 @@ async def lastpost(ctx, username, channel_id: int, role_id: int, bot, logger, la
                 likes = post.likes
                 date = post.date
 
-
                 if username == last_post_author:
                     last_post = date
                     logger.info("lastpost called on same author as post checker, adjusting last_post value")
-                    logger.info("Time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ " (EST)\n")
-
+                    logger.info("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " (EST)\n")
 
                 if post.typename == "GraphSidecar":
                     sidecar_nodes = list(post.get_sidecar_nodes())
@@ -50,9 +57,8 @@ async def lastpost(ctx, username, channel_id: int, role_id: int, bot, logger, la
                 else:
                     top_image_url = post.url
 
-                
                 e = discord.Embed(
-                    title = f"New Instagram post published on {date}",
+                    title=f"New Instagram post published on {date}",
                     description=caption,
                     url=post_url,
                     color=0x40E0D0,
@@ -60,103 +66,116 @@ async def lastpost(ctx, username, channel_id: int, role_id: int, bot, logger, la
 
                 e.set_image(url=top_image_url)
                 e.set_footer(text=f"❤️ {likes} | 💬 {comments}")
-                e.set_author(name=username, icon_url = profile_pic_url, url="https://www.instagram.com/" + username)
+                e.set_author(
+                    name=username,
+                    icon_url=profile_pic_url,
+                    url="https://www.instagram.com/" + username,
+                )
 
                 role = discord.utils.get(ctx.guild.roles, id=role_id)
                 logger.info("Manually sent last post")
-                logger.info("Time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ " (EST)\n")
+                logger.info("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " (EST)\n")
                 await target_channel.send(f" {role.mention} New post on {fullname}'s Instagram!", embed=e)
-
 
                 return last_post
 
         logger.info(f"No unpinned posts found for {username}.")
-        logger.info("Time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ " (EST)\n")
+        logger.info("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " (EST)\n")
         await ctx.send(f"No unpinned posts found for {username}.")
 
         return last_post
 
     except Exception as e:
         logger.error(f"An error occurred: {e}")
-        logger.info("Time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ " (EST)\n")
+        logger.info("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " (EST)\n")
         await ctx.send(f"An error occurred: {e}")
 
 
+async def post_check(
+    bot,
+    logger,
+    last_post,
+    last_post_author,
+    global_ctx,
+    global_username,
+    global_channel_id,
+    global_role_id,
+):
 
-async def post_check(bot, logger, last_post, last_post_author, global_ctx, global_username, global_channel_id, global_role_id):
+    last_post_author = global_username
 
-        last_post_author = global_username
+    target_channel = bot.get_channel(global_channel_id)
 
+    L = instaloader.Instaloader()
 
-        target_channel = bot.get_channel(global_channel_id)
+    try:
+        profile = instaloader.Profile.from_username(L.context, global_username)
 
-        L = instaloader.Instaloader()
+        profile_pic_url = profile.profile_pic_url
+        fullname = profile.full_name
 
-        try:
-            profile = instaloader.Profile.from_username(L.context, global_username)
+        for post in profile.get_posts():
+            if not post.is_pinned:
 
-            profile_pic_url = profile.profile_pic_url
-            fullname = profile.full_name
-            
-            for post in profile.get_posts():
-                if not post.is_pinned:
+                # first iteration
+                if last_post == None:
+                    last_post = post.date
+                    logger.info("No new post found (first iteration)")
+                    logger.info("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " (EST)\n")
+                    return
+                # new post
+                elif last_post != post.date:
+                    last_post = post.date
 
-                    # first iteration
-                    if last_post == None:                   
-                        last_post = post.date
-                        logger.info("No new post found (first iteration)")
-                        logger.info("Time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ " (EST)\n")
-                        return
-                    # new post
-                    elif last_post != post.date:
-                        last_post = post.date
+                    post_url = f"https://www.instagram.com/p/{post.shortcode}/"
+                    caption = post.caption
+                    comments = post.comments
+                    likes = post.likes
+                    date = post.date
 
-                        post_url = f"https://www.instagram.com/p/{post.shortcode}/"
-                        caption = post.caption
-                        comments = post.comments
-                        likes = post.likes
-                        date = post.date
-
-                        if post.typename == "GraphSidecar":
-                            sidecar_nodes = list(post.get_sidecar_nodes())
-                            if sidecar_nodes:
-                                top_image_url = sidecar_nodes[0].display_url
-                            else:
-                                top_image_url = None
+                    if post.typename == "GraphSidecar":
+                        sidecar_nodes = list(post.get_sidecar_nodes())
+                        if sidecar_nodes:
+                            top_image_url = sidecar_nodes[0].display_url
                         else:
-                            top_image_url = post.url
-
-                        
-                        e = discord.Embed(
-                            title = f"New Instagram post published on {date}",
-                            description=caption,
-                            url=post_url,
-                            color=0x40E0D0,
-                        )
-
-                        e.set_image(url=top_image_url)
-                        e.set_footer(text=f"❤️ {likes} | 💬 {comments}")
-                        e.set_author(name=global_username, icon_url = profile_pic_url, url="https://www.instagram.com/" + global_username)
-
-                        # get channel next
-                        logger.info("New Post Found")
-                        logger.info("Time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ " (EST)\n")
-
-                        role2 = discord.utils.get(global_ctx.guild.roles, id=global_role_id)
-
-                        await target_channel.send(f" {role2.mention} New post on {fullname}'s Instagram!", embed=e)
-                
-                        #role = discord.utils.get(ctx.guild.roles, name='Discord Manager')
-                        
-                        #await channel.send(embed=e)
-                        #await ctx.send(embed=e)
-
-                        return
+                            top_image_url = None
                     else:
-                        logger.info("Most recent post is not new. No new posts found")
-                        logger.info("Time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ " (EST)\n")
-                        return
+                        top_image_url = post.url
 
-            await logger.info(f"No unpinned posts found for {global_username}.\n")
-        except Exception as e:
-            await logger.error(f"An error occurred: {e}\n")
+                    e = discord.Embed(
+                        title=f"New Instagram post published on {date}",
+                        description=caption,
+                        url=post_url,
+                        color=0x40E0D0,
+                    )
+
+                    e.set_image(url=top_image_url)
+                    e.set_footer(text=f"❤️ {likes} | 💬 {comments}")
+                    e.set_author(
+                        name=global_username,
+                        icon_url=profile_pic_url,
+                        url="https://www.instagram.com/" + global_username,
+                    )
+
+                    # get channel next
+                    logger.info("New Post Found")
+                    logger.info("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " (EST)\n")
+
+                    role2 = discord.utils.get(global_ctx.guild.roles, id=global_role_id)
+
+                    await target_channel.send(f" {role2.mention} New post on {fullname}'s Instagram!", embed=e)
+
+                    # role = discord.utils.get(ctx.guild.roles, name='Discord Manager')
+
+                    # await channel.send(embed=e)
+                    # await ctx.send(embed=e)
+
+                    return
+                else:
+                    logger.info("Most recent post is not new. No new posts found")
+                    logger.info("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " (EST)\n")
+                    return
+
+        await logger.info(f"No unpinned posts found for {global_username}.\n")
+    except Exception as e:
+        await logger.error(f"An error occurred: {e}\n")
--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,8 @@
-import os
 import logging
-from dotenv import load_dotenv
+import os
 from logging.config import dictConfig
 
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -11,46 +11,37 @@ DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
 LOGGING_CONFIG = {
     "version": 1,
     "disabled_existing_loggers": False,
-    "formatters":{
-        "verbose":{
-            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
-        },
-        "standard":{
-            "format": "%(levelname)-10s - %(name)-15s : %(message)s"
-        }
+    "formatters": {
+        "verbose": {"format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"},
+        "standard": {"format": "%(levelname)-10s - %(name)-15s : %(message)s"},
     },
-    "handlers":{
+    "handlers": {
         "console": {
-            'level': "DEBUG",
-            'class': "logging.StreamHandler",
-            'formatter': "standard"
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
         },
         "console2": {
-            'level': "WARNING",
-            'class': "logging.StreamHandler",
-            'formatter': "standard"
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
         },
         "file": {
-            'level': "INFO",
-            'class': "logging.FileHandler",
-            'filename': "logs/infos.log",
-            'mode': "w",
-            'formatter': "verbose"
-        }
-    },
-    "loggers":{
-        "bot": {
-            'handlers': ['console'],
             "level": "INFO",
-            "propagate": False
+            "class": "logging.FileHandler",
+            "filename": "logs/infos.log",
+            "mode": "w",
+            "formatter": "verbose",
         },
+    },
+    "loggers": {
+        "bot": {"handlers": ["console"], "level": "INFO", "propagate": False},
         "discord": {
-            'handlers': ['console2', "file"],
+            "handlers": ["console2", "file"],
             "level": "INFO",
-            "propagate": False
-        }
-    }
+            "propagate": False,
+        },
+    },
 }
 
 dictConfig(LOGGING_CONFIG)
-
